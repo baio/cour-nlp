d8425924fac389eec92aa61a11bd21bc37f9d7b8
baio <max.putilov@gmail.com>
2013-05-02 12:36:46 +0400

    fix

--- hw3/tests/test_ibm1.py
+++ hw3/tests/test_ibm1.py
@@ -8,10 +8,25 @@ class TestIbm1(unittest.TestCase):
 
 
     def test_initialize_debug(self):
-        res = align_lines(["the dog", "the cat", "the rat", "some thing"],
+        als = align_lines(["the dog", "the cat", "the rat", "some thing"],
                          ["athe adog", "bthe bcat", "athe crat", "asome athing"])
-        res
+        with open("data/dev.out", "w") as fs:
+            for i_al in xrange(len(als)):
+                al = als[i_al]
+                for i_w in xrange(len(al)):
+                    fs.write("{} {} {}\n".format( i_al + 1, al[i_w], i_w + 1))
 
+    def test_dev_out(self):
+        return
+        with open("data/dev.en") as fs:
+            e = fs.read().split("\n")
+        with open("data/dev.es") as fs:
+            f = fs.read().split("\n")
+        als = align_lines(e, f)
+        with open("data/dev.out", "w") as fs:
+            for al in als:
+                fs.write(" ".join(map(lambda x: str(x), al)))
+                fs.write("\n")
 
 if __name__ == '__main__':
     unittest.main()
